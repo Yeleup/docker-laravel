9aeaa0ad4638cff62290af668ba112bd2e39c862
Magzhan Yeleup <magzhan9292@gmail.com>
2023-07-30 18:24:41 +0600

    JWT

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
