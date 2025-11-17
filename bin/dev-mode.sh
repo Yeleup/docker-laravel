02d7c37a445eb6423e099d6eea44eb5c2df45336
Magzhan Yeleup <magzhan9292@gmail.com>
2025-11-17 19:07:25 +0500

    Добавлено примерное окружение и обновлены пути в Dockerfile и docker-compose

--- bin/dev-mode.sh
+++ bin/dev-mode.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-XDEBUG_MODE=${XDEBUG_MODE:-off} docker compose -f docker-compose.yaml -f docker-compose.dev.yaml --env-file ./app/.env up "$@"
+XDEBUG_MODE=${XDEBUG_MODE:-off} docker compose -f docker-compose.yaml -f docker-compose.dev.yaml --env-file=.env up "$@"
